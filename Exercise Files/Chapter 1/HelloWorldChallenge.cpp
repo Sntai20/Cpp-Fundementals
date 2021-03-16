5ae995b5131030837e6d9d29228cca810b65e14e
Antonio Santana <antonio.santana.ir@outlook.com>
2021-03-16 09:01:47 -0700

    Added cpp essentials files

---
+++ Exercise Files/Chapter 1/HelloWorldChallenge.cpp
@@ -0,0 +1,14 @@
+//Hello World
+/* By Erin Colvin */
+//iostream used for inputting and outputting
+#include <iostream>
+using namespace std;
+
+
+//////////////////////////////////////
+//Main is always the first to run
+/////////////////////////////////////
+int main()
+{
+	cout << "hello world this is Daisy" << endl;
+}
---
+++ HBD/HBD.cpp
@@ -0,0 +1,18 @@
+//Happy Birthday to demonstrate functions
+//by Antonio Santana
+#include <iostream> 
+using namespace std; 
+ 
+void PrintHBD();
+int main()
+{
+	PrintHBD();
+    PrintHBD();
+    cout << "Happy Birthday dear user\n";
+    PrintHBD();
+}
+
+void PrintHBD()
+{
+    cout << "Happy Birthday to you\n";
+}
